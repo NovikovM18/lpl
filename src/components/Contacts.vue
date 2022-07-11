87e42c68c3391b9c4d710496cacda0637f91b8a7
NovikovM <novikov1807@gmail.com>
2022-07-11 18:58:28 +0300

    add forms input

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -4,9 +4,10 @@
     <div class="contacts__g2"></div>
     <div class="box contacts__box">
       <h1 class="contacts__title">Contact Us</h1>
-      <form 
+      <form
         class="form"
-        @submit=""
+        ref="contactForm"
+        @submit.prevent="submitForm"
       >
         <div class="form__fb">
           <input 
@@ -14,12 +15,16 @@
             class="form__input" 
             placeholder="Your Name"
             v-model.trim="form.name"
+            @blur="v$.form.name.$touch"
+            :class="{ error: v$.form.name.$error }"
           >
           <input 
             type="text" 
             class="form__input" 
             placeholder="Email Address"
             v-model.trim="form.email"
+            @blur="v$.form.email.$touch"
+            :class="{ error: v$.form.email.$error }"
           >
         </div>
         <div class="form__sb">
@@ -28,22 +33,54 @@
             class="form__input" 
             placeholder="Phone Number"
             v-model.trim="form.phone"
+            @blur="v$.form.phone.$touch"
+            :class="{ error: v$.form.phone.$error }"
           >
-          <input 
-            type="text" 
-            class="form__input" 
-            placeholder="Choose your job type"
-            v-model="form.job"
-          >
+          <div class="custom-select" :tabindex="tabindex" @blur="open = false">
+            <div class="selected" :class="{ open: open }" @click="open = !open">
+              {{ form.job }}
+            </div>
+            <div class="items" :class="{ selectHide: !open }">
+              <div
+                v-for="(option, i) of options"
+                :key="i"
+                @click="
+                  form.job = option;
+                  open = false;
+                "
+              >
+                {{ option }}
+              </div>
+            </div>
+          </div>
+
         </div>
         <textarea 
           class="form__ta" 
           placeholder="Messege"
           v-model.trim="form.messege"
+          @blur="v$.form.messege.$touch"
+          :class="{ error: v$.form.messege.$error }"
         >
         </textarea>
         <div class="form__tb">
-          <input type="file" class="form__file">
+          <input 
+            type="file" 
+            class="form__file_i"
+            id="file_i"
+            @change="onFileChange"
+          >
+          <label 
+            for="file_i"
+            class="form__file"
+          >
+            <img
+              class="form__file_img"
+              src="../assets/img/input_file.svg" 
+              alt="icon"
+            >
+            <p class="form__file_p">Upload your CV</p>
+          </label>
           <button 
             class="form__btn"
             type="submit"
@@ -57,16 +94,67 @@
 </template>
 
 <script>
+import useVuelidate from '@vuelidate/core'
+import { required, email, numeric } from '@vuelidate/validators'
 export default {
+  setup () {
+    return { v$: useVuelidate() }
+  },
   data() {
     return {
+      options: ['Create', 'Fix', 'Search', 'Verification'],
+      default: 'Choose your job type',
+      tabindex: 0,
+      selected: 'Choose your job type',
+      open: false,
       form: {
         name: '',
         email: '',
         phone: '',
-        job: '',
+        job: 'Choose your job type',
         messege: '',
+        cv: null,
       }
+    }
+  },
+  validations() {
+    return{
+      form: {
+        name: { required },
+        email: { required, email },
+        phone: { required, numeric },
+        job: { required },
+        messege: { required },
+      }
+    }
+  },
+  methods: {
+    async submitForm () {
+      const isFormCorrect = await this.v$.$validate()
+      if(isFormCorrect) {
+        alert('Thanks for you messege!');
+        console.log(this.form);
+        this.$refs.contactForm.reset();
+        this.form.job = 'Choose your job type';
+      } else {
+        return
+      }
+    },
+    onFileChange(e) {
+      let files = e.target.files || e.dataTransfer.files;
+      if (!files.length)
+        return;
+      this.createFile(files[0]);
+    },
+    createFile(file) {
+      let newFile = new File();
+      var reader = new FileReader();
+      var vm = this;
+
+      reader.onload = (e) => {
+        vm.form.cv = e.target.result;
+      };
+      reader.readAsDataURL(file);
     }
   }
 }
@@ -210,12 +298,28 @@ export default {
       font-weight: 400;
       font-size: 20px;
       line-height: 23px;
-      text-align: center;
       color: #101020;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 12px;
         @media (max-width: 768px) {
           width: 335px;
           height: 50px;
         }
+      &_img {
+        width: 24px;
+        height: 24px;
+      }
+      &_p {
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 23px;
+        color: #101020;
+      }
+      &_i {
+        display: none;
+      }
     }
     &__btn {
       width: 284px;
@@ -235,5 +339,79 @@ export default {
           line-height: 23px;
         }
     }
+  }
+  .error {
+    border: 2px dashed red;
+  }
+
+  .custom-select {
+    position: relative;
+    width: 430px;
+    height: 64px;
+    text-align: left;
+    outline: none;
+    line-height: 64px;
+    font-size: 20px;
+      @media (max-width: 768px) {
+        width: 335px;
+        height: 50px;
+        font-size: 16px;
+        line-height: 50px;
+      }
+  }
+
+  .custom-select .selected {
+    background-color: #fff;
+    border-radius: 7px;
+    box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.15);
+    color: #585858;
+    font-weight: 300;
+    padding-left: 1em;
+    cursor: pointer;
+    user-select: none;
+  }
+
+  .custom-select .selected.open {
+    border: 1px solid #F8F8F8;
+    border-radius: 7px 7px 0px 0px;
+  }
+
+  .custom-select .selected:after {
+    position: absolute;
+    content: "⌵";
+    top: 0;
+    right: 20px;
+    width: 0;
+    height: 0;
+  }
+
+  .custom-select .items {
+    font-weight: 300;
+    color: #585858;
+    border-radius: 0px 0px 6px 6px;
+    overflow: hidden;
+    border-right: 1px solid #F8F8F8;
+    border-left: 1px solid #F8F8F8;
+    border-bottom: 1px solid #F8F8F8;
+    position: absolute;
+    background-color: #fff;
+    left: 0;
+    right: 0;
+    z-index: 1;
+  }
+
+  .custom-select .items div {
+    color: #585858;
+    padding-left: 1em;
+    cursor: pointer;
+    user-select: none;
+  }
+
+  .custom-select .items div:hover {
+    background-color: #F8F8F8;
+  }
+
+  .selectHide {
+    display: none;
   }
 </style>
